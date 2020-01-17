97306d87e13fe0591f47f3f32737922704509e6c
suphachai <boyphone99@gmail.com>
2020-01-17 16:51:16 +0700

    Initial commit

---
+++ app/src/main/java/com/androiddeft/loginandregistration/ProfileActivity.java
@@ -0,0 +1,36 @@
+package com.androiddeft.loginandregistration;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ProfileActivity extends AppCompatActivity {
+    private SessionHandler session;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        session = new SessionHandler(getApplicationContext());
+        User user = session.getUserDetails();
+        TextView welcomeText = findViewById(R.id.welcomeText1);
+
+        welcomeText.setText("Welcome "+user.getFullName()+", your session will expire on "+user.getSessionExpiryDate());
+
+        Button logoutBtn = findViewById(R.id.btnLogout1);
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                session.logoutUser();
+                Intent i = new Intent(ProfileActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/androiddeft/loginandregistration/URLs.java
@@ -0,0 +1,10 @@
+package com.androiddeft.loginandregistration;
+
+public class URLs {
+
+            //private static final String ROOT_URL = "http://10.0.2.2/finalproject/api/";
+            private static final String ROOT_URL = "https://finaltest001.000webhostapp.com/api/";
+
+    public static final String REG_URL = "http://10.0.2.2/finalproject/api/register.php";
+    public static final String LOGIN_URL = "http://10.0.2.2/finalproject/api/login.php";
+}
